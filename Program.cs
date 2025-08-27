93cf00d64bd3d82ca72f9e2e0b169b9ca69f604b
Vanessa Macchiavello <vanessam9263@gmail.com>
2025-08-27 14:54:47 +0200

    Checkup solution

--- Program.cs
+++ Program.cs
@@ -1,11 +1,23 @@
+﻿static string Greet(string namePassedIn)
+{
+  return "Welcome to SALT, " + namePassedIn;
+}
+
+static string DateToCourse(DateTime courseDate)
+{
+  int daysLeft = (courseDate - DateTime.Today).Days;
+  return "Today it is " + daysLeft + "days left until the course starts.";
+}
+
 Console.Write("What is your name? ");
 var name = Console.ReadLine()!;
 
+Console.Write("When does that course start? ");
+var currentDate = Console.ReadLine()!;
+DateTime courseDate = DateTime.Parse(currentDate);
+
+var countDown = DateToCourse(courseDate);
 var greeting = Greet(name);
 
 Console.WriteLine(greeting);
-
-﻿static string Greet(string namePassedIn)
-{
-  return "Welcome to SALT, ";
-}
+Console.WriteLine(countDown);
