5aa727194da4eb5d7770f31376f4782d05dd7371
Vanessa Macchiavello <vanessam9263@gmail.com>
2025-08-30 17:17:05 +0200

    added the other birthyears iwth same method

--- dotnet-trainingGround/TrainingGround.Tests/PersonTests.cs
+++ dotnet-trainingGround/TrainingGround.Tests/PersonTests.cs
@@ -1,3 +1,5 @@
+namespace TrainingGround.Tests;
+
 public class PersonTests
 {
   [Fact]
@@ -22,26 +24,26 @@ public class PersonTests
   }
 
   [Fact]
-    public void create_person_using_constructor_with_birthyear()
-    {
-        var p = new Person(1972);
-        Assert.NotNull(p);
-        Assert.Equal(1972, p.BirthYear);
-    }
+  public void create_person_using_constructor_with_birthyear()
+  {
+    var p = new Person(1972);
+    Assert.NotNull(p);
+    Assert.Equal(1972, p.BirthYear);
+  }
 
-    [Fact]
-    public void someone_born_1972_is_50_in_2022()
-    {
-        var p = new Person(1972);
-        var age = p.GetAge(2022);
-        Assert.Equal(50, age);
-    }
+  [Fact]
+  public void someone_born_1972_is_50_in_2022()
+  {
+    var p = new Person(1972);
+    var age = p.GetAge(2022);
+    Assert.Equal(50, age);
+  }
 
-    [Fact]
-    public void someone_born_2022_is_0_in_2022()
-    {
-        var p = new Person(2022);
-        var age = p.GetAge(2022);
-        Assert.Equal(0, age);
-    }
+  [Fact]
+  public void someone_born_2022_is_0_in_2022()
+  {
+    var p = new Person(2022);
+    var age = p.GetAge(2022);
+    Assert.Equal(0, age);
+  }
 }
