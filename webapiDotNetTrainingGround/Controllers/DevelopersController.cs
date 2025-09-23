c07e421c2ce8542de57872eefc2b9b73621ef4b8
Vanessa Macchiavello <vanessam9263@gmail.com>
2025-09-23 13:24:48 +0200

    added developerscontroller

---
+++ webapiDotNetTrainingGround/Controllers/DevelopersController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+using webapiDotNetTrainingGround.Models;
+
+namespace webapiDotNetTrainingGround.Controllers;
+
+[ApiController]
+[Route("/api/[controller]")]
+public class DevelopersController : ControllerBase
+{
+    private List<Developer> _db;
+
+    public DevelopersController()
+    {
+        _db = new List<Developer>();
+    }
+}
