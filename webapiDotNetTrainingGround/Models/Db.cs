983b3770681ef144817c04e93104e8b660cfe559
Vanessa Macchiavello <vanessam9263@gmail.com>
2025-09-23 14:46:42 +0200

    added bd class

---
+++ webapiDotNetTrainingGround/Models/Db.cs
@@ -0,0 +1,11 @@
+public class Db
+{
+    public Db()
+    {
+        Developers = new List<Developer>() {
+            new Developer() { Id = 1, Name = "Marcus", Email = "marcus@salt.dev" },
+            new Developer() { Id = 2, Name = "Vanessa", Email = "bea@salt.dev" },
+        };
+    }
+    public List<Developer> Developers { get; set; }
+}
