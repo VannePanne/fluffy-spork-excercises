6f0c99963f62d99de31f25eb735fc2176a412627
Vanessa Macchiavello <vanessam9263@gmail.com>
2025-09-23 15:36:14 +0200

    CDR fix

--- webapiDotNetTrainingGround/Models/CreateDeveloperRequest.cs
+++ webapiDotNetTrainingGround/Models/CreateDeveloperRequest.cs
@@ -1,5 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace webapiDotNetTrainingGround.Models;
+
 public class CreateDeveloperRequest
 {
-    public string? Name { get; set; }
-    public string? Email { get; set; }
+    [Required]
+    public string? Name { get; set; } = default!;
+    [Required]
+    public string? Email { get; set; } = default!;
 }
---
+++ webapiDotNetTrainingGround/Models/DeveloperResponse.cs
@@ -0,0 +1,8 @@
+namespace webapiDotNetTrainingGround.Dtos;
+
+public class DeveloperResponse
+{
+    public int Id       { get; set; }
+    public string Name  { get; set; } = default!;
+    public string Email { get; set; } = default!;
+}
