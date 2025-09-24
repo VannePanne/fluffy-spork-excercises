8b554f7c61bbb96aeedf08da206ab7c27ee6316f
Vanessa Macchiavello <vanessam9263@gmail.com>
2025-09-24 13:53:03 +0200

    Creating mvc application

---
+++ mvcDotNetTrainingGround/Controllers/DevelopersController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+using mvcDotNetTrainingGround.Models;
+namespace mvcDotNetTrainingGround.Controllers;
+
+public class DevelopersController : Controller
+{
+  private Db _db;
+  public DevelopersController(Db db)
+  {
+    _db = db;
+  }
+
+  public IActionResult Index()
+  {
+    return View(_db.Developers);
+  }
+}
---
+++ mvcDotNetTrainingGround/Models/Db.cs
@@ -0,0 +1,15 @@
+namespace mvcDotNetTrainingGround.Models;
+
+public class Db
+{
+    public Db()
+    {
+        Developers = new List<Developer>
+        {
+            new Developer { Id = 1, Name = "Marcus",  Email = "marcus@salt.dev" },
+            new Developer { Id = 2, Name = "Vanessa", Email = "bea@salt.dev"    },
+        };
+    }
+
+    public List<Developer> Developers { get; set; }
+}
