a8e06256ea3e0ba1110f16a0041fb8aa361925aa
Vanessa Macchiavello <vanessam9263@gmail.com>
2025-09-25 11:51:46 +0200

    details page done

--- AdressBook/Controllers/AdressBookController.cs
+++ AdressBook/Controllers/AdressBookController.cs
@@ -5,13 +5,18 @@ namespace AdressBook.Controllers;
 
 public class AdressBookController : Controller
 {
-  private Db _db;
-  public AdressBookController(Db db)
+    private Db _db;
+    public AdressBookController(Db db)
     {
         _db = db;
     }
-  public IActionResult Index()
+    public IActionResult Index()
     {
         return View(_db.Adresses);
     }
+    public IActionResult Details(int id){
+        var adress = _db.Adresses.Find(d => d.Id == id);
+        return View(adress);
+    }
+
 }
